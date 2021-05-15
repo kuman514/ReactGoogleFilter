b8f689f8ef89e9e49ce0c26e3f1c08e27b895c25
kumankoishi.py <7iggif@naver.com>
2021-05-16 07:43:47 +0900

    Added ExceptSite Component

--- google-filter-react/src/App.tsx
+++ google-filter-react/src/App.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import './App.css';
 import Primary from './components/Primary';
 import Strong from './components/Strong';
+import ExceptSite from './components/ExceptSite'
 
 function App() {
   let pri: Primary;
   let str: Strong;
+  let ecs: ExceptSite;
 
   return (
     <div className="App">
       <Primary ref={(primaryComponent) => {pri = primaryComponent as Primary}}></Primary>
-      <Strong ref={(strongContent) => {str = strongContent as Strong}}></Strong>
+      <Strong ref={(strongComponent) => {str = strongComponent as Strong}}></Strong>
+      <ExceptSite ref={(exceptSiteComponent) => {ecs = exceptSiteComponent as ExceptSite}}></ExceptSite>
       <button onClick={(e) => {
         e.preventDefault();
         // get primary content
@@ -23,7 +26,12 @@ function App() {
         for (const item of str.getContent()) {
           finalStrong += `${item} `;
         }
-        window.open(`https://www.google.com/search?q=${finalPrimary}${finalStrong}`, '_blank');
+        // get except site content
+        let finalExcSite: string = '';
+        for (const item of ecs.getContent()) {
+          finalExcSite += `${item} `;
+        }
+        window.open(`https://www.google.com/search?q=${finalPrimary}${finalStrong}${finalExcSite}`, '_blank');
       }}>검색하기</button>
     </div>
   );
---
+++ google-filter-react/src/components/ExceptSite.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Component } from 'react';
+
+interface ExceptSiteProps {
+
+}
+
+interface ExceptSiteState {
+  content: string;
+}
+
+class ExceptSite extends Component<ExceptSiteProps, ExceptSiteState> {
+  constructor(props: ExceptSiteProps) {
+    super(props);
+    this.state = {
+      content: ''
+    }
+  }
+
+  getContent = (): string[] => {
+    return this.state.content.split(',').map((item) => {
+      return `-site:${item.trim()}`;
+    });
+  }
+
+  public render(): JSX.Element {
+    return (
+      <div>
+        <h2>제외할 사이트</h2>
+        <input type="text" name="except-site" id="except-site" onChange={(e) => {
+          e.preventDefault();
+          this.setState({
+            content: e.target.value
+          });
+        }}/>
+      </div>
+    );
+  }
+}
+
+export default ExceptSite;
