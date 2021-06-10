9bc82e5999fb007d3a4707e3c6d71e0283dbc47e
kumankoishi.py <7iggif@naver.com>
2021-06-11 00:13:08 +0900

    Modified ThemeChanger

--- google-filter-react/src/components/ThemeChanger.tsx
+++ google-filter-react/src/components/ThemeChanger.tsx
@@ -2,21 +2,19 @@ import React from 'react';
 
 function ThemeChanger(): JSX.Element {
   return (
-    <div className="ThemeChanger">
-      <button onClick={() => {
-        const curTheme: string | null = document.documentElement.getAttribute('color-theme');
-        switch (curTheme) {
-          case 'dark':
-            document.documentElement.setAttribute('color-theme', 'light');
-            break;
-          case 'light':
-            document.documentElement.setAttribute('color-theme', 'dark');
-            break;
-        }
-      }}>
-        테마 바꾸기
-      </button>
-    </div>
+    <button className="ThemeChanger" onClick={() => {
+      const curTheme: string | null = document.documentElement.getAttribute('color-theme');
+      switch (curTheme) {
+        case 'dark':
+          document.documentElement.setAttribute('color-theme', 'light');
+          break;
+        case 'light':
+          document.documentElement.setAttribute('color-theme', 'dark');
+          break;
+      }
+    }}>
+      테마 바꾸기
+    </button>
   );
 }
 
