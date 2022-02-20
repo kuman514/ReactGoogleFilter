86a8ef8dc5c8deedcba4f9c5e6aece7d834d59ba
kumankoishi.py <7iggif@naver.com>
2022-02-21 01:16:42 +0900

    Revised Primary

--- google-filter-react/src/components/Primary.tsx
+++ google-filter-react/src/components/Primary.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Component } from 'react';
+import { useDispatch } from 'react-redux';
 
+/*
 interface PrimaryProps {
 
 }
@@ -8,36 +8,26 @@ interface PrimaryProps {
 interface PrimaryState {
   content: string
 }
+*/
 
-class Primary extends Component<PrimaryProps, PrimaryState> {
-  constructor(props: PrimaryProps) {
-    super(props);
-    this.state = {
-      content: ''
-    }
-  }
-  
-  getContent = (): string[] => {
-    return this.state.content.split(',').map((item) => {
-      return item.trim();
-    }).filter((item) => item !== '');
-  }
+function Primary(): JSX.Element {
+  const dispatch = useDispatch();
 
-  public render(): JSX.Element {
-    return (
-      <div className="Primary">
-        <div>
-          <h2>검색어</h2>
-        </div>
-        <input type="text" name="primary" id="primary" placeholder="콤마(,)로 구분하여 여러개 입력 가능." onChange={(e) => {
-          e.preventDefault();
-          this.setState({
-            content: e.target.value
-          });
-        }}/>
+  return (
+    <div className="Primary">
+      <div>
+        <h2>검색어</h2>
       </div>
-    );
-  }
+      <input type="text" name="primary" id="primary" placeholder="콤마(,)로 구분하여 여러개 입력 가능." onChange={(e) => {
+        e.preventDefault();
+        console.log(e.target.value);
+        dispatch({
+          type: 'SETPRIMARY',
+          payload: e.target.value
+        });
+      }}/>
+    </div>
+  );
 }
 
 export default Primary;
