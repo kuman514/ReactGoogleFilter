ba2280a981ced54d0287c714ec5228aa3364e514
kumankoishi.py <7iggif@naver.com>
2022-02-21 01:16:55 +0900

    Revised ExceptSite

--- google-filter-react/src/components/ExceptSite.tsx
+++ google-filter-react/src/components/ExceptSite.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Component } from 'react';
+import { useDispatch } from 'react-redux';
 
+/*
 interface ExceptSiteProps {
 
 }
@@ -8,36 +8,26 @@ interface ExceptSiteProps {
 interface ExceptSiteState {
   content: string
 }
+*/
 
-class ExceptSite extends Component<ExceptSiteProps, ExceptSiteState> {
-  constructor(props: ExceptSiteProps) {
-    super(props);
-    this.state = {
-      content: ''
-    }
-  }
+function ExceptSite(): JSX.Element {
+  const dispatch = useDispatch();
 
-  getContent = (): string[] => {
-    return this.state.content.split(',').map((item) => {
-      return item.trim();
-    }).filter((item) => item !== '');
-  }
-
-  public render(): JSX.Element {
-    return (
-      <div className="ExceptSite">
-        <div>
-          <h2>제외할 사이트</h2>
-        </div>
-        <input type="text" name="except-site" id="except-site" placeholder="도메인 네임 형태(예: namu.wiki)로 입력. 콤마(,)로 구분하여 여러개 입력 가능." onChange={(e) => {
-          e.preventDefault();
-          this.setState({
-            content: e.target.value
-          });
-        }}/>
+  return (
+    <div className="ExceptSite">
+      <div>
+        <h2>제외할 사이트</h2>
       </div>
-    );
-  }
+      <input type="text" name="except-site" id="except-site" placeholder="도메인 네임 형태(예: namu.wiki)로 입력. 콤마(,)로 구분하여 여러개 입력 가능." onChange={(e) => {
+        e.preventDefault();
+        console.log(e.target.value);
+        dispatch({
+          type: 'SETEXCEPT',
+          payload: e.target.value
+        });
+      }}/>
+    </div>
+  );
 }
 
 export default ExceptSite;
