9f3749d07cb205c6ea6a2d2b67287b899902aad4
kumankoishi.py <7iggif@naver.com>
2022-02-21 01:17:15 +0900

    Revised Strong

--- google-filter-react/src/components/Strong.tsx
+++ google-filter-react/src/components/Strong.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Component } from 'react';
+import { useDispatch } from 'react-redux';
 
+/*
 interface StrongProps {
 
 }
@@ -8,36 +8,26 @@ interface StrongProps {
 interface StrongState {
   content: string
 }
+*/
 
-class Strong extends Component<StrongProps, StrongState> {
-  constructor(props: StrongProps) {
-    super(props);
-    this.state = {
-      content: ''
-    }
-  }
+function Strong(): JSX.Element {
+  const dispatch = useDispatch();
 
-  getContent = (): string[] => {
-    return this.state.content.split(',').map((item) => {
-      return item.trim();
-    }).filter((item) => item !== '');
-  }
-
-  public render(): JSX.Element {
-    return (
-      <div className="Strong">
-        <div>
-          <h2>정확히 포함시킬 검색어</h2>
-        </div>
-        <input type="text" name="strong" id="strong" placeholder="콤마(,)로 구분하여 여러개 입력 가능." onChange={(e) => {
-          e.preventDefault();
-          this.setState({
-            content: e.target.value
-          });
-        }}/>
+  return (
+    <div className="Strong">
+      <div>
+        <h2>정확히 포함시킬 검색어</h2>
       </div>
-    );
-  }
+      <input type="text" name="strong" id="strong" placeholder="콤마(,)로 구분하여 여러개 입력 가능." onChange={(e) => {
+        e.preventDefault();
+        console.log(e.target.value);
+        dispatch({
+          type: 'SETSTRONG',
+          payload: e.target.value
+        });
+      }}/>
+    </div>
+  );
 }
 
 export default Strong;
